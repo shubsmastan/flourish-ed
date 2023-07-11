5df3e9f847762391a483fc2d4072747b334b8c7a
Shoaib Mastan <114753586+IceCoffee426@users.noreply.github.com>
2023-07-11 16:13:46 +0100

    Fix bug with student sort

--- src/app/dashboard/assessment/[cid]/page.tsx
+++ src/app/dashboard/assessment/[cid]/page.tsx
@@ -92,7 +92,7 @@ const ClassAssessment = ({ params }: { params: { cid: string } }) => {
 
   const filteredStudents = students.sort((a, b) => {
     if (a.name < b.name) return -1;
-    if (b.name < a.name) return -1;
+    if (b.name < a.name) return 1;
     return 0;
   });
 
