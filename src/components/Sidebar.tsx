9778c7508da74d4142c88655cae6a73725eeca9d
Shoaib Mastan <114753586+shubsmastan@users.noreply.github.com>
2024-06-12 13:41:47 +0100

    New routes for classes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useMutation, useQuery } from '@tanstack/react-query';
+import Link from 'next/link';
 import axios from 'axios';
 import { useState } from 'react';
 
@@ -15,7 +16,7 @@ export const Sidebar = () => {
 		queryKey: ['classes'],
 		queryFn: async () => {
 			const { data } = await axios.get(
-				`http://localhost:3000/api/classes`
+				`${process.env.NEXT_PUBLIC_BASE_URL}/api/classes`
 			);
 			return data;
 		},
@@ -47,10 +48,19 @@ export const Sidebar = () => {
 		<nav
 			className={`${
 				open ? 'translate-x-0' : '-translate-x-64'
-			} flex h-[calc(100vh-3.5rem)] w-64 flex-col p-4 text-sm shadow-md transition-transform duration-200 sm:sticky sm:translate-x-0`}>
+			} flex h-[calc(100vh-3.5rem)] w-64 flex-col p-2 border-r-[0.5px] text-sm shadow-md transition-transform duration-200 sm:sticky sm:translate-x-0`}>
 			<ul>
+				<li>
+					<Link href={`/dashboard/classes/today`}>Today</Link>
+				</li>
 				{classes.map((cls: any) => {
-					return <li key={cls._id}>{cls.name}</li>;
+					return (
+						<li key={cls._id}>
+							<Link href={`/dashboard/classes/${cls._id}`}>
+								{cls.name}
+							</Link>
+						</li>
+					);
 				})}
 			</ul>
 		</nav>
